ab8d3a1b75bd4ecef363b5f8d4d3eb8061283b99
max <maxcheung@optusnet.com.au>
2024-09-01 16:07:16 +1000

    main add metrics

--- my-cloudwatch-metrics/test/my-cloudwatch-dashboard.test.ts
+++ my-cloudwatch-metrics/test/my-cloudwatch-dashboard.test.ts
@@ -1,9 +1,9 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as MyCloudwatchDashboard from '../lib/my-cloudwatch-dashboard-stack';
+// import * as MyCloudwatchDashboard from '../lib/my-cloudwatch-metrics-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/my-cloudwatch-dashboard-stack.ts
+// example resource in lib/my-cloudwatch-metrics-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
