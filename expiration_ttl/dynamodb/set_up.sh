b1fa2f4d55dcc6020f70d016072eca6cf58b6601
mxcheung <mxcheung@users.noreply.github.com>
2024-10-28 07:45:49 +1100

    Update set_up.sh

--- expiration_ttl/dynamodb/set_up.sh
+++ expiration_ttl/dynamodb/set_up.sh
@@ -4,7 +4,7 @@ aws dynamodb create-table \
     --table-name ExpiringRecordsTable \
     --attribute-definitions AttributeName=id,AttributeType=S \
     --key-schema AttributeName=id,KeyType=HASH \
-    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=1000 \
     --stream-specification StreamEnabled=true,StreamViewType=KEYS_ONLY
 
 aws dynamodb update-time-to-live \
