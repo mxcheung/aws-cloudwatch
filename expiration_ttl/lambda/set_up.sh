49c87bf6923ddbb854c32712bf071a8dfaf5f1d4
mxcheung <mxcheung@users.noreply.github.com>
2024-10-27 21:45:45 +1100

    Create set_up.sh

---
+++ expiration_ttl/lambda/set_up.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+
+
+
+aws lambda create-function \
+    --function-name CountExpiredRecords \
+    --runtime python3.9 \
+    --role arn:aws:iam::$AWS_ACCOUNT_ID:role/LambdaDynamoDBStreamRole \
+    --handler lambda_function.lambda_handler \
+    --zip-file fileb://function.zip \
+    --environment Variables="{TABLE_NAME=ExpiringRecordsTable}"
