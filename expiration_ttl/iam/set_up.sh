8546a25d15584b81a8ff67f3419f0bbaf2f99f83
mxcheung <mxcheung@users.noreply.github.com>
2024-10-27 21:38:28 +1100

    Update set_up.sh

--- expiration_ttl/iam/set_up.sh
+++ expiration_ttl/iam/set_up.sh
@@ -3,3 +3,12 @@
 aws iam create-role \
     --role-name LambdaDynamoDBStreamRole \
     --assume-role-policy-document file://trust-policy.json
+
+
+aws iam attach-role-policy \
+    --role-name LambdaDynamoDBStreamRole \
+    --policy-arn arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess
+
+aws iam attach-role-policy \
+    --role-name LambdaDynamoDBStreamRole \
+    --policy-arn arn:aws:iam::aws:policy/CloudWatchFullAccess
