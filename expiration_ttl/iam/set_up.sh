7b2a3bd843f0de759eb8099f75f63fb21da0a1ca
mxcheung <mxcheung@users.noreply.github.com>
2024-10-28 23:20:41 +1100

    Update set_up.sh

--- expiration_ttl/iam/set_up.sh
+++ expiration_ttl/iam/set_up.sh
@@ -4,6 +4,8 @@ aws iam create-role \
     --role-name LambdaDynamoDBStreamRole \
     --assume-role-policy-document file://trust-policy.json
 
+# Wait for the IAM role to be created
+aws iam wait role-exists --role-name LambdaDynamoDBStreamRole
 
 aws iam attach-role-policy \
     --role-name LambdaDynamoDBStreamRole \
