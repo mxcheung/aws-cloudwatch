b5050dfd2c1b85bf3b81b6d6d098faf014e3c46f
mxcheung <mxcheung@users.noreply.github.com>
2024-10-29 06:51:46 +1100

    Update set_up.sh

--- expiration_poll/lambda/set_up.sh
+++ expiration_poll/lambda/set_up.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+export TABLE_NAME="ExpiringRecordsTable"
+export INDEX_NAME="ExpiryIndex"
+export SNS_TOPIC_NAME="ExpiryAlertTopic"
+export LAMBDA_FUNCTION_NAME="CheckExpiredRecordsFunction"
+export LAMBDA_ROLE_NAME="LambdaDynamoDBAccessRole"
+export EXPIRY_THRESHOLD=100  # Set your threshold value here
+
+
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 
 zip function.zip lambda_function.py
@@ -7,10 +15,13 @@ zip function.zip lambda_function.py
 # Wait for the IAM role to be created
 aws iam wait role-exists --role-name LambdaDynamoDBStreamRole
 
+export LAMBDA_ROLE_ARN=$(aws iam get-role --role-name $LAMBDA_ROLE_NAME --query 'Role.Arn' --output text)
+
 aws lambda create-function \
-    --function-name CountExpiredRecords \
-    --runtime python3.9 \
-    --role arn:aws:iam::$AWS_ACCOUNT_ID:role/LambdaDynamoDBStreamRole \
-    --handler lambda_function.lambda_handler \
+    --function-name $LAMBDA_FUNCTION_NAME \
     --zip-file fileb://function.zip \
-    --environment Variables="{TABLE_NAME=ExpiringRecordsTable}"
+    --handler lambda_function.lambda_handler \
+    --runtime python3.9 \
+    --role $LAMBDA_ROLE_ARN \
+    --environment Variables="{TABLE_NAME=$TABLE_NAME,INDEX_NAME=$INDEX_NAME,SNS_TOPIC_ARN=$SNS_TOPIC_ARN,EXPIRY_THRESHOLD=$EXPIRY_THRESHOLD}"
+
