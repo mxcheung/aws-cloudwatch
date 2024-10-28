4de386021074bb124ffb0686fb1c7b93c8dc3fe8
mxcheung <mxcheung@users.noreply.github.com>
2024-10-28 23:11:40 +1100

    Update set_up.sh

--- expiration_ttl/data_stream/set_up.sh
+++ expiration_ttl/data_stream/set_up.sh
@@ -2,6 +2,10 @@
 
 STREAM_ARN=$(aws dynamodb describe-table --table-name ExpiringRecordsTable --query "Table.LatestStreamArn" --output text)
 
+# Step 2: Wait for the Lambda function to be created
+aws lambda wait function-exists --function-name CountExpiredRecords
+
+
 aws lambda create-event-source-mapping \
     --function-name CountExpiredRecords \
     --event-source-arn $STREAM_ARN \
