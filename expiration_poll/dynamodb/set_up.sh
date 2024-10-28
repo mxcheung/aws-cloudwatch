9a4d3333192479a205fcdf31b3c3fae8689f91d4
mxcheung <mxcheung@users.noreply.github.com>
2024-10-29 06:42:17 +1100

    Update set_up.sh

--- expiration_poll/dynamodb/set_up.sh
+++ expiration_poll/dynamodb/set_up.sh
@@ -5,5 +5,5 @@ aws dynamodb create-table \
     --table-name ExpiringRecordsTable \
     --attribute-definitions AttributeName=TransactionId,AttributeType=S AttributeName=status,AttributeType=S AttributeName=expiry,AttributeType=N \
     --key-schema AttributeName=TransactionId,KeyType=HASH \
-    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=1000 \
     --global-secondary-indexes "IndexName=ExpiryIndex,KeySchema=[{AttributeName=status,KeyType=HASH},{AttributeName=expiry,KeyType=RANGE}],Projection={ProjectionType=ALL},ProvisionedThroughput={ReadCapacityUnits=5,WriteCapacityUnits=5}"
