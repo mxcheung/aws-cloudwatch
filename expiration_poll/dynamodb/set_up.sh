735134de57e47eaf718294661359aefb6155a0f1
mxcheung <mxcheung@users.noreply.github.com>
2024-10-29 06:30:25 +1100

    Update set_up.sh

--- expiration_poll/dynamodb/set_up.sh
+++ expiration_poll/dynamodb/set_up.sh
@@ -7,12 +7,3 @@ aws dynamodb create-table \
     --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=1000 \
     --stream-specification StreamEnabled=true,StreamViewType=NEW_AND_OLD_IMAGES
 
-aws dynamodb update-time-to-live \
-    --table-name ExpiringRecordsTable \
-    --time-to-live-specification "Enabled=true, AttributeName=expiry"    
-
-
-# Increase the write capacity to a higher level, e.g., 1000 WriteCapacityUnits
-# aws dynamodb update-table \
-#    --table-name ExpiringRecordsTable \
-#    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=1000
