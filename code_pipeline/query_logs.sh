fb70244fe0eabc5af9f3fda23abea9f765f89a87
mxcheung <mxcheung@users.noreply.github.com>
2025-01-10 13:00:55 +1100

    Update query_logs.sh

--- code_pipeline/query_logs.sh
+++ code_pipeline/query_logs.sh
@@ -1,2 +1,7 @@
 # Fetch log group names that start with 'aws/codebuild/aa'
 LOG_GROUPS=$(aws logs describe-log-groups --log-group-name-prefix 'aws/codebuild/aa' --query 'logGroupNames' --output text)
+
+
+```
+aws logs describe-log-groups --log-group-name-prefix 'aws/codebuild' --query 'logGroupNames' --output json | python3 -c "import sys, jq; print('\n'.join(jq.first('.[]', sys.stdin)))"
+```
